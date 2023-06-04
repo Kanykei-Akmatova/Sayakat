76fa00c45fac44da0e3ab2547ae44850cc88e6e9
Kanykei-Akmatova <kanykeyakmatova@gmail.com>
2023-06-04 13:48:40 -0400

    several updates for syakat project

--- SayakatApp/saykat/urls.py
+++ SayakatApp/saykat/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('saykat/bookings', views.bookings, name='saykat'),
+    path('saykat/bookings/add', views.add_bookings, name='saykat'),
 ]
--- SayakatApp/saykat/views.py
+++ SayakatApp/saykat/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Booking
+from .models import Booking, Package
 
 
 def bookings(request):
@@ -9,5 +9,16 @@ def bookings(request):
     template = loader.get_template('bookings_list.html')
     context = {
         'bookings_list': bookings_list,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def add_bookings(request):
+    package_list = Package.objects.all()
+
+    template = loader.get_template('bookings_add.html')
+    context = {
+        'customer': 1,
+        'package_list': package_list,
     }
     return HttpResponse(template.render(context, request))
