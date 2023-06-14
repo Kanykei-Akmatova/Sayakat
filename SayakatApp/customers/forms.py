e3188f24624e99380e753ca894b7601144e843ce
Kanykei-Akmatova <kanykeyakmatova@gmail.com>
2023-06-13 22:41:39 -0400

    Clean registry form

--- SayakatApp/customers/forms.py
+++ SayakatApp/customers/forms.py
@@ -8,6 +8,12 @@ class UserRegistrationForm(UserCreationForm):
     last_name = forms.CharField(max_length=101)
     email = forms.EmailField()
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = visible.field.label
+
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
